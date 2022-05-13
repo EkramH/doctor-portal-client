1d3955f8f1bc7ee7acd9d44c4e321ab9d27b54f2
EkramH <ekramhosen329@gmail.com>
2022-05-13 12:27:41 +0600

    daisy UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Doctor portal</h1>
+    <div>
+      <h1 className="text-8xl text-center">Doctor portal</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
