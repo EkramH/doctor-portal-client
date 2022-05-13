0e79b5d91990a1913d0cef6695f6649654d68acf
EkramH <ekramhosen329@gmail.com>
2022-05-13 18:43:20 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Navbar from "./shared/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-8xl text-center">Doctor portal</h1>
-      <button className="btn btn-primary">Button</button>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
