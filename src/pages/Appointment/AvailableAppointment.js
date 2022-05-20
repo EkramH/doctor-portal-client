c3a9063aa685b125e48ceb10db413c199e756d6d
EkramH <ekramhosen329@gmail.com>
2022-05-21 02:41:06 +0600

    get available slots

--- src/pages/Appointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment.js
@@ -6,9 +6,10 @@ import Service from "./Service";
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
+  const formattedDate = format(date || new Date(), "PP");
 
   useEffect(() => {
-    fetch("http://localhost:5000/service")
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
